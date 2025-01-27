2a2d02e8b9b76a226bc5c668d4144886f1f46593
akshay7805 <akshaygahlyan49@gmail.com>
2025-01-27 15:25:28 +0530

    Add files via upload

---
+++ alarm.py
@@ -0,0 +1,18 @@
+import time 
+from datetime import datetime
+def set_alarm(alarm_time):
+    try:
+
+        alarm_time=datetime.strptime(alarm_time,"%H:%M:%S").time()
+        print(f"Alarm set for {alarm_time}")
+
+        while True:
+            current_time=datetime.now().time()
+            if current_time>=alarm_time:
+                print("Alarm is ringing Please wake up!")
+                break
+            time.sleep(1)
+    except ValueError:
+        print("Invalid format please enter vlaid format")
+set_alarm("15:17:00")                
+          
